2818034b210a01f766ed8459e81de6062a95ed8d
vivmost <vivmost@gmail.com>
2022-02-14 12:32:46 +0530

    product api added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts, createProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 module.exports = router
